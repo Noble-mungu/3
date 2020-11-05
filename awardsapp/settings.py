e3fb568784a7fa724912dd66595242dad2f4cdb5
Noble <mungunoble@gmail.com>
2020-11-05 15:10:02 +0300

    deployment

--- awardsapp/settings.py
+++ awardsapp/settings.py
@@ -19,7 +19,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
-
+MODE=config("MODE", default="dev")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
