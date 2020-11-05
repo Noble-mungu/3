31125f3e28647ecef6eef8ff45971535feb8571d
Noble <mungunoble@gmail.com>
2020-11-05 14:05:30 +0300

    deployment

--- awardsapp/settings.py
+++ awardsapp/settings.py
@@ -18,8 +18,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODE="dev"
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 
 # Quick-start development settings - unsuitable for production
