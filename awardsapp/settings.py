ba6c9875349e7fb8b5acd8910ff46b8fc93e5cb8
Noble <mungunoble@gmail.com>
2020-11-05 15:13:09 +0300

    deployment

--- awardsapp/settings.py
+++ awardsapp/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
        
 }
